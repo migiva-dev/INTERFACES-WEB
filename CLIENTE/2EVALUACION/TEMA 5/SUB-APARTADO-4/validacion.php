22d40207e57dbb591d831acedd2d738327b508d4
Miguel25604 <miguel.gavila@alu.ceacfp.es>
2026-03-03 19:57:21 +0100

    d

--- CLIENTE/2EVALUACION/TEMA 5/SUB-APARTADO-4/validacion.php
+++ CLIENTE/2EVALUACION/TEMA 5/SUB-APARTADO-4/validacion.php
@@ -9,16 +9,5 @@ $pass = "";
 $mensaje = "";
 $desarrolladores = []
 
-try {
-
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $pass
-    );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    if ($_SE
 
 ?>
